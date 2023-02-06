760dde1f8e103ce8564b9c28b887785993d4b618
Ivan Danilau <ivdanilov96@gmail.com>
2023-02-06 15:57:20 +0400

    task #1071 Greatest Common Divisor of Strings

---
+++ src/main/java/com/gmail/ivdanilov96/leetcode/tasks/easy/GreatestCommonDivisorOfStrings.java
@@ -0,0 +1,36 @@
+package com.gmail.ivdanilov96.leetcode.tasks.easy;
+
+import com.gmail.ivdanilov96.leetcode.tasks.Task;
+import com.gmail.ivdanilov96.leetcode.tasks.TaskNum;
+
+@Task(TaskNum.T_1071)
+public class GreatestCommonDivisorOfStrings {
+
+  public String gcdOfStrings(String first, String second) {
+    int gcd = getGCD(first.length(), second.length());
+    String divisor = first.substring(0, gcd);
+    if (correctDivisor(first, divisor, gcd) && correctDivisor(second, divisor, gcd)) {
+      return divisor;
+    }
+    return "";
+  }
+
+  private int getGCD(int numOne, int numTwo) {
+    if (numTwo == 0) {
+      return numOne;
+    } else {
+      return getGCD(numTwo, numOne % numTwo);
+    }
+  }
+
+  private boolean correctDivisor(String str, String divisor, int gcd) {
+    for (int i = 0; i < str.length() / gcd; i++) {
+      if (!str.startsWith(divisor, gcd * i)) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
+
+
---
+++ src/test/java/com/gmail/ivdanilov96/leetcode/tasks/easy/GreatestCommonDivisorOfStringsTest.java
@@ -0,0 +1,29 @@
+package com.gmail.ivdanilov96.leetcode.tasks.easy;
+
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+import java.util.stream.Stream;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+@DisplayName("Task #1071 - Greatest Common Divisor of Strings")
+class GreatestCommonDivisorOfStringsTest {
+
+  @ParameterizedTest
+  @MethodSource("initParameters")
+  void gcdOfStrings(String strOne, String strTwo, String expected) {
+    var task = new GreatestCommonDivisorOfStrings();
+    Assertions.assertEquals(expected, task.gcdOfStrings(strOne, strTwo));
+  }
+
+  private static Stream<Arguments> initParameters() {
+    return Stream.of(
+        arguments("ABCABC", "ABC", "ABC"),
+        arguments("ABABAB", "ABAB", "AB"),
+        arguments("LEET", "CODE", "")
+    );
+  }
+}
